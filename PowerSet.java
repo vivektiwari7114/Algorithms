4df5bbf0b250c1ec4c4597d411e28d823eede482
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-09-01 14:21:35 -0700

    Updated Power Set

---
+++ PowerSet.java
@@ -0,0 +1,40 @@
+package practise.com;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+
+public class PowerSet {
+	public static void main(String args[]){
+		int s[] = {1,2};
+		
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		
+		
+		for(int i=0; i< s.length; i++){
+			//System.out.println("fr "+result);
+			List<List<Integer>> temp = new ArrayList<List<Integer>>();
+			//System.out.println("fr2 "+result);
+			for(List<Integer> t : result)
+				 temp.add(new ArrayList<Integer>(t));
+			for(List<Integer> t : temp)
+				t.add(s[i]);
+			//System.out.println("fr3 "+result);
+			//System.out.println("Temp "+temp);
+			ArrayList<Integer> tmp = new ArrayList<Integer>();
+			tmp.add(s[i]);
+			temp.add(tmp);
+			result.addAll(temp);
+			//System.out.println("temp "+temp);
+			//System.out.println("result "+result);
+			
+		}
+		result.add(new ArrayList<Integer>());
+		System.out.println(result);
+		
+		
+	}
+
+}
