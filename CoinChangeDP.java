010574e4b6d863f6d477447b5063165c69d492b4
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-09-03 14:28:23 -0700

    Coin Change

---
+++ CoinChangeDP.java
@@ -0,0 +1,28 @@
+package practise.com;
+
+
+/* Name: Calculate the ways to make N cents using denomination
+ * Parameters: NA
+ * Return :Number of paths 
+ * Function: Calculate the ways to make N cents using denomination
+ */
+public class CoinChangeDP {
+	public static void main(String args[]){
+		int s[] = {1,2,3};
+		int amount = 4;
+		int noOfCoins = s.length;
+		System.out.println(getNumberOfWays(s, noOfCoins, amount));
+		
+	}
+	public static  int getNumberOfWays(int s[], int noOfCoins , int amount){
+		
+		if(amount == 0)
+			return 1;
+		if(amount < 0 || (noOfCoins <= 0 && amount > 0 ))
+			return 0;
+		return (getNumberOfWays(s,noOfCoins,amount - s[noOfCoins - 1] ) 
+				+getNumberOfWays(s,noOfCoins-1,amount  ) );
+		
+	}
+
+}
