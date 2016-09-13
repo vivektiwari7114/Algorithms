9c8f9c0eeea0565ee99fc964600f10076ece288e
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-09-13 01:23:03 -0700

    Updated MergerSort

--- prg/src/practise/com/MergeSort.java
+++ prg/src/practise/com/MergeSort.java
@@ -6,30 +6,29 @@ package practise.com;
  * Function: Sort Array in increasing order using merge Sort
  */
 public class MergeSort {
-	int  temp[] = new int[4];
+	
 	
 	public static void main(String args[]){
-		int a[] = {10,9,8,7};
+		int a[] = {1,10,4,32,61,11,23,100};
 		int low = 0;
 		int high = a.length - 1;
+		int temp[] = new int [a.length];
+		
 		MergeSort m = new MergeSort();
-		m.mergeSort(a,low,high);
-		for(int t: m.temp)
+		m.mergeSort(a, temp, low, high);
+		for(int t: temp)
 			System.out.print(t + " ");
 			
 	}
-	public  void mergeSort(int a[], int low, int high){
-		
+	public  void mergeSort(int a[], int temp[], int low, int high){
 		if(low == high)
 			return;
 		int mid = low +(high -low)/2;
-		
-		mergeSort(a, low, mid);
-		mergeSort(a, mid+1, high);
-		merge(a, low,mid+1, high);
-		
+		mergeSort(a, temp,  low, mid);
+		mergeSort(a, temp, mid+1, high);
+		merge(a, temp, low,mid+1, high);
 	}
-	public  void merge(int a[], int low, int center, int high){
+	public  void merge(int a[], int temp[], int low, int center, int high){
 		
 		int left = low;
 		int i = low;
@@ -37,24 +36,17 @@ public class MergeSort {
 		int right = center;
 		int rightEnd = high;
 		while(left <= leftEnd && right <= rightEnd){
-			if(a[left] < a[right]){
-				temp[i++] = a[left];
-				left++;
-			}else{
-				temp[i++] = a[right];
-				right++;
+			if(a[left] < a[right])
+				temp[i++] = a[left++];
+			else
+				temp[i++] = a[right++];
 			}
-			
-		}
-		while(left <= leftEnd){
-			temp[i++] = a[left];
-			left++;
-			
-		}
-		while(right <= rightEnd){
-			temp[i++] = a[right];
-			right++;
-		}
+		
+		while(left <= leftEnd)
+			temp[i++] = a[left++];
+		while(right <= rightEnd)
+			temp[i++] = a[right++];
+		//Transfer to the Array a
 		for(int k= low; k <= high;k++)
 			a[k] = temp[k];
 			
