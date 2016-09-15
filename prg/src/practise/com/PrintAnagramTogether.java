5d1c38ead39f87b76e8ee5628782b643e358ab24
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-09-15 05:50:55 -0700

    Print all the Anagram in the given string together

--- prg/src/practise/com/PrintAnagramTogether.java
+++ prg/src/practise/com/PrintAnagramTogether.java
@@ -2,21 +2,39 @@ package practise.com;
 
 import java.util.Arrays;
 
+class Anagram{
+	int index;
+	String str;
+	public Anagram(int index , String str){
+		this.index = index;
+		this.str = str;
+	}
+	public void setString(String st){
+		this.str = st;
+	}
+}
+
 public class PrintAnagramTogether {
 	public static void main(String args[]){
 		String input[] = {"cat", "dog", "tac", "god", "act"};
 		int l = input.length;
+		Anagram in[] = new Anagram[l];
 		
-		int index[] = new int[l];
-		String word[] = new String[l];
+		for(int i=0;i < l;i++)
+			in[i] = new Anagram(i, input[i]);
+		//Sorting the individual Strings
+		for(int i=0; i< l; i++)
+			sortIndividualWords(in[i]);
 		
-		for(int i =0 ; i < l; i++){
-			index[i] = i;
-			input[i] = sortIndividualWords(input[i]);
-		}
+		//Sort the entire Words 
+		sortTheArray(in);
+			
+		/*for(Anagram a: in)
+			System.out.print(a.str + " " + a.index);*/
 		
-		for(int i=0; i< l;i++)
-			System.out.println(input[i]);
+		for(int i=0; i<l;i++)
+			System.out.print(input[in[i].index] + " " );
+		
 		
 		
 	
@@ -24,9 +42,24 @@ public class PrintAnagramTogether {
 		
 		
 	}
-	public static String sortIndividualWords(String input){
-		char in[] = input.toCharArray();
-		Arrays.sort(in);
-		return new String(in);
+	public static Anagram[] sortTheArray(Anagram[] in){
+		for(int i=0; i< in.length; i++){
+			for(int j=i+1; j < in.length; j++){
+				if( in[i].str.compareTo(in[j].str) > 0){
+					Anagram temp = in[i];
+					in[i] = in[j];
+					in[j] = temp;
+					}
+			}
+		}
+		return in;
 	}
+	public static Anagram sortIndividualWords(Anagram input){
+		char in[] = input.str.toCharArray();
+		Arrays.sort(in);
+		input.setString(String.valueOf(in));
+		return input;
+		
+	}
+	
 }
