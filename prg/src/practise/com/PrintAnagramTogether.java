3b39796f9a92841c8971ed60e40c6e806bc7e2ef
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-09-15 16:56:30 -0700

    Optimized for time complexity : Not success

--- prg/src/practise/com/PrintAnagramTogether.java
+++ prg/src/practise/com/PrintAnagramTogether.java
@@ -1,7 +1,9 @@
 package practise.com;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.ArrayList;
 
 class Anagram{
@@ -19,8 +21,8 @@ class Anagram{
 public class PrintAnagramTogether {
 	public static void main(String args[]){
 		String input[] = {"cat", "dog", "tac", "god", "act"};
-		List<List<String>> fin = new ArrayList<List<String>>();
-		List<String> temp = new ArrayList<String>();
+		List<String> tp = new ArrayList<String>();
+		List<List<String>> result = new ArrayList<List<String>>();
 		int l = input.length;
 		Anagram in[] = new Anagram[l];
 		
@@ -30,42 +32,51 @@ public class PrintAnagramTogether {
 		for(int i=0; i< l; i++)
 			sortIndividualWords(in[i]);
 		//Sort the entire Words 
-		sortTheArray(in);
+		sortTheArray(in,input,tp,result);
 		/*for(int i=0; i<l;i++)
 			System.out.print(input[in[i].index] + " " );
 		System.out.println("");*/
 		
-		String comp = in[0].str;
+		/*String comp = in[0].str;
 		temp.add(input[in[0].index]);
 		System.out.println(comp);
 				for(int i=1; i<l;i++){
 				    if( comp.compareTo( in[i].str ) == 0 )
 				        temp.add(input[in[i].index]);
 				  else{
-					 // System.out.println("hello");
 				      fin.add(new ArrayList(temp));
 				      temp.clear();
 				      comp = in[i].str;
-				     // System.out.println(comp);
 				      temp.add(input[in[i].index]);
 				  }
 				}
 				fin.add(temp);
 				
-				System.out.println(fin);
+				System.out.println(fin);*/
+		//System.out.println(result);
 		
 }
-	public static Anagram[] sortTheArray(Anagram[] in){
-		for(int i=0; i< in.length; i++){
-			for(int j=i+1; j < in.length; j++){
-				if( in[i].str.compareTo(in[j].str) > 0){
-					Anagram temp = in[i];
-					in[i] = in[j];
-					in[j] = temp;
+	public static void sortTheArray(Anagram[] in, String input[], List<String> tp, List<List<String>> result){
+		Set<String> store  = new HashSet<String>();
+		
+		for(int i =0; i < in.length; i++){
+			String comp = in[i].str;
+			if(store.contains(comp))
+				continue;
+			else
+				store.add(comp);
+			tp.add(input[in[i].index]);
+			for(int j=i+1; j< in.length;j++){
+				if( comp.compareTo(in[j].str)== 0  ){
+					tp.add(input[in[j].index]);
 				}
 			}
+			result.add(new ArrayList(tp));
+			System.out.println("Intermediate Result"+result);
+			tp.clear();
 		}
-		return in;
+		
+		
 	}
 	public static Anagram sortIndividualWords(Anagram input){
 		char in[] = input.str.toCharArray();
