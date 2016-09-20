b6653055a1c3d610139dec545932bbf5affa5e79
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-09-20 02:13:03 -0700

    implement Divide operator using +

--- prg/src/practise/com/mathOperatorUsingAdd.java
+++ prg/src/practise/com/mathOperatorUsingAdd.java
@@ -2,10 +2,10 @@ package practise.com;
 
 public class mathOperatorUsingAdd {
 	public static void main(String args[]){
-		int a = -4;
-		int b = 0;
+		int a = 0;
+		int b = -2;
 		//System.out.println(subtract(a, b));
-		System.out.println(multiply(a, b));
+		System.out.println(divide(a, b));
 		
 	}
 	public static int multiply(int a , int b){
@@ -29,6 +29,27 @@ public class mathOperatorUsingAdd {
 			a = a + d;
 		}
 		return sum;
+	}
+	public static int  divide(int a , int b){
+		if(b ==0)
+			return -1;
+		int divd = a < 0?negate(a): a;
+		int div = b < 0? negate(b): b;
+		
+		if(divd < div)
+				return 0;
+		int count = 0;
+		
+		while( divd >= div ){
+			divd = divd - div;
+			count++;
+		}
+		if(a<0 && b < 0)
+				return count;
+		if(a < 0 || b < 0)
+				return negate(count);
+		return count;
+		
 	}
 
 }
