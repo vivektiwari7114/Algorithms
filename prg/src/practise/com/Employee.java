d81ad17b80177dad874b0776ff51ebff77de8704
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-09-30 11:20:31 -0700

    Private Constructor

---
+++ prg/src/practise/com/Employee.java
@@ -0,0 +1,45 @@
+package practise.com;
+
+public class Employee {
+	private int age;
+	private String name;
+	private Employee(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+	public static void main(String args[]){
+		Student s1 = Student.getObject(12, "Tripti");
+		s1.print(s1);
+		
+	}
+}
+
+class Student{
+	private int roll;
+	private String sName;
+	public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	private Student(int roll, String sName){
+		this.roll = roll;
+		this.sName = sName;
+	}
+	public static Student getObject(int roll, String name){
+		return new Student(roll, name);
+	}
+	public static void  print(Student s1){
+		System.out.println(s1.roll);
+		System.out.println(s1.sName);
+		
+	}
+}
+
