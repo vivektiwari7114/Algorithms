29e98afcc98d19aad998698247b5e8faf43f1a7c
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-10-01 11:24:34 -0700

    Generic example

---
+++ prg/src/practise/com/BasicGeneric.java
@@ -0,0 +1,39 @@
+package practise.com;
+
+public class BasicGeneric {
+	public static void main(String args[]){
+		GenericExample<String> go = new GenericExample<String>("Hello");
+		go.printGeneric();
+		
+		GenericExample<Integer> go1 = new GenericExample<Integer>(48);
+		go1.printGeneric();
+		
+		GenericExample<Integer> go2 = new GenericExample<Integer>(new Integer(89));
+		go2.printGeneric();
+		
+		GenericExample<Float> go3 = new GenericExample<Float>(new Float(89.87));
+		go3.printGeneric();
+		
+		
+		GenericExample<Double> go4 = new GenericExample<Double>(new Double(89.87));
+		go4.printGeneric();
+		
+		GenericExample<Double> go5 = new GenericExample<Double>(678.89);
+		go5.printGeneric();
+		
+		
+		GenericExample<Boolean> go6 = new GenericExample<Boolean>(new Boolean(true));
+		go6.printGeneric();
+	}
+	
+
+}
+class GenericExample<T>{
+	T refObject;
+	public GenericExample(T param){
+		this.refObject = param;
+	}
+	public void printGeneric(){
+		System.out.println(this.refObject);
+	}
+}
