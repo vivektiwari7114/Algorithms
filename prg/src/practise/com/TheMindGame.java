17e0127bcdcade3fcddf47ff9dc9d67cf0464279
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-10-18 16:50:07 -0700

    Mind Array With different logic

--- prg/src/practise/com/TheMindGame.java
+++ prg/src/practise/com/TheMindGame.java
@@ -5,6 +5,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/* Name: The Mind game
+ * Parameters: Not Applicable
+ * Return :Not Applicable
+ * Function: Guess number of hits and Psuedo hits in the the given array combination
+ */
+
 public class TheMindGame {
 	public static void main(String args[]){
 		String sol = "RYGB";
