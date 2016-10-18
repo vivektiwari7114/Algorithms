a138d77e95fc25ef1f1308bbef8db619fb0e9b00
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-10-17 20:42:53 -0700

    Maximum of two number

--- prg/src/practise/com/FindMax.java
+++ prg/src/practise/com/FindMax.java
@@ -1,5 +1,17 @@
 package practise.com;
 
 public class FindMax {
+	public static void main(String args[]){
+		int a = 5;
+		int b = 10;
+		int c = a-b;
+		System.out.println();
+		
+		int k = (c >> 31) & 0x1;
+		System.out.println(k);
+		int max = a-k*c;
+		System.out.println("Max "+max);
+	}
+	
 
 }
