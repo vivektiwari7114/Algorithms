d91e0972a623479cf5b2a2c57bae1c18fa7f8ea9
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-10-24 14:56:41 -0700

    RandomShuffle of an array

---
+++ prg/src/practise/com/RandomShuffleArray.java
@@ -0,0 +1,37 @@
+package practise.com;
+
+import java.util.Random;
+
+/* Name: Shuffle a  array randomly
+ * Parameters: Not Applicable
+ * Return :Not Applicable
+ * Function: Shuffle a  array randomly
+ */
+public class RandomShuffleArray {
+
+	public static void main(String[] args) {
+		//Object array[] = {'a','b','c','d','e'};
+		Object array[] = {"a","b","c","d","e"};
+		RandomShuffleArray.shuffle(array);
+		
+
+	}
+	public static void shuffle(Object array[]){
+		if(array == null || array.length == 0)
+				return;
+		int length = array.length;
+		Random rm = new Random();
+		
+		for(int i=0; i < length; i++){
+			int randomNumber = rm.nextInt(length);
+			Object temp = array[randomNumber];
+			array[randomNumber] = array[i];
+			array[i] = temp;
+		}
+		
+		//Display Array
+		for(Object t : array)
+				System.out.print(t+" ");
+	}
+
+}
