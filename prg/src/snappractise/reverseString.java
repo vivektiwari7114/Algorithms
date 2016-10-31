f4a4fd94547c5cde71932953300fc499bd44b410
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-10-31 12:59:30 -0700

    Reverse a STring

---
+++ prg/src/snappractise/reverseString.java
@@ -0,0 +1,21 @@
+package snappractise;
+
+public class reverseString {
+	public static void main(String args[]){
+		String s = "abcd";
+		char t[] = s.toCharArray();
+		System.out.println( reverseString(t) );
+	}
+	public static  String reverseString(char[] t){
+		int l = t.length;
+		int i=0, j = l-1;
+		while(i < j){
+			char temp = t[i];
+			t[i] = t[j];
+			t[j] = temp;
+			i++;
+			j--;
+		}
+		return new String(t);
+	}
+}
