504689d75eb190ce0517ae27fc99948ac96f0222
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-10-31 23:01:52 -0700

    Convert bits into another

--- prg/src/snappractise/BitMasking.java
+++ prg/src/snappractise/BitMasking.java
@@ -2,20 +2,21 @@ package snappractise;
 
 public class BitMasking {
 		public static void main(String args[]){
-			int n = 0b10000000000;
+			int n = 0b10001111100;
 			int m = 0b10101;
 			int i = 2;
 			int j = 6;
+			int num =0;
+			int max = ~0;
+			int left = max - ((1 << j+1) - 1);
+			//System.out.println(Integer.toBinaryString(left));
 			
-			while(i <= j){
-				int bit = m & 0b1;
-				int temp = bit << i;
-				n = n | temp;
-				i++;
-				m = m >> 1;
-				
-			}
-			System.out.println(Integer.toBinaryString(n));
+			int right = ((1 << i) - 1);
+			//System.out.println(Integer.toBinaryString(right));
+			int mask = left | right;
+			num = (n & mask)| (m << i);
+			
+			System.out.println(Integer.toBinaryString(num));
 		}
 
 }
