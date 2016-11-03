039f3ddbf2f8bc62743c9e0836801d4c8d35122e
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-11-02 18:10:38 -0700

    Iterate values of HashSet

---
+++ prg/src/practise/com/Iterators.java
@@ -0,0 +1,30 @@
+package practise.com;
+
+import java.util.*;
+
+public class Iterators {
+
+	public static void main(String[] args) {
+	
+		Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
+		mp.put(1, 100);
+		mp.put(2, 200);
+		mp.put(3, 300);
+		mp.put(4, 400);
+		mp.put(5, 500);
+		
+		
+		/*for(Map.Entry<Integer, Integer> entry : mp.entrySet()){
+			int key = entry.getKey();
+			int val = entry.getValue();
+			System.out.println(key + " "+val);
+			
+		}*/
+		
+		
+		
+		
+
+	}
+
+}
