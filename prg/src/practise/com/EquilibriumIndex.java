fb8e8ed8ceb2a5c14884411ca30bc9b83cae960a
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-11-07 08:05:49 -0800

    Brute Force Approach n2 complexity

--- prg/src/practise/com/EquilibriumIndex.java
+++ prg/src/practise/com/EquilibriumIndex.java
@@ -1,13 +1,13 @@
 package practise.com;
 
-/* Name: Equilibrium Index
+/* Name: Equilibrium Index Brute Force
  * Parameters: Not Applicable
  * Return :Not Applicable
  * Function: Given an array find the equilibrium index
  */
 class EquilibriumIndex {
 	public static void main(String args[]){
-		int A[] = {0, -2147483648, -2147483648};
+		int A[] = {0, 1,2,3,4,5,6 };
 		System.out.println(new EquilibriumIndex().solution(A));
 	}
 	
