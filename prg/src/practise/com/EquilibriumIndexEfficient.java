cf99472e2b86ff5fd1faa545dc516af73245adcf
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-11-07 08:11:22 -0800

    Equilibrium Index
    Efficient implementation of Equilibrium Index

---
+++ prg/src/practise/com/EquilibriumIndexEfficient.java
@@ -0,0 +1,30 @@
+package practise.com;
+
+public class EquilibriumIndexEfficient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array[] ={-7,1,5,2,-4,3,0};
+		int index = getEquilibriumIndex(array);
+		System.out.println(index);
+
+	}
+	public static int getEquilibriumIndex(int array[]){
+		
+		int l = array.length;
+		int sum = 0;
+		int leftSum = 0;
+		for(int i=0; i < l;i++)
+				sum = sum + array[i];
+		
+		for(int i=0; i < l ; i++){
+			sum = sum - array[i];
+			if(sum == leftSum)
+					return i;
+			leftSum = leftSum + array[i];
+		}
+		return -1;
+		
+	}
+
+}
