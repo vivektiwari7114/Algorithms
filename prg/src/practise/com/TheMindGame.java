eb67432b65d892bcf23267733d858d92b9d438e6
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-12-25 02:16:07 +0530

    Hits and Psudo Hits

--- prg/src/practise/com/TheMindGame.java
+++ prg/src/practise/com/TheMindGame.java
@@ -13,39 +13,44 @@ import java.util.Map;
 
 public class TheMindGame {
 	public static void main(String args[]){
-		String sol = "YYGB";
-		String guess = "GGGB";
-		Map<Character,Integer> m1 = new HashMap<Character,Integer>();
-		List<Character> l1 = new ArrayList<Character>();
 		
-		char a1[] = sol.toCharArray();
-		char a2[] = guess.toCharArray();
-		
-		int hit =0;
+		String first = "GGGB";
+		String second = "RRGB";
+		int hit = 0;
 		int psHit = 0;
+		ArrayList<Character> l1 = new ArrayList<Character>();
+		Map<Character, Integer> mp = new HashMap<Character, Integer>();
 		
-		for(int i=0; i< a1.length;  i++){
-			if(a1[i] == a2[i])
+		char one[] = first.toCharArray();
+		char two[] = second.toCharArray();
+		
+		for(int i=0; i < 4; i++){
+			if(one[i] == two[i])
 				hit++;
 			else{
-				l1.add(a1[i]);
-				if(m1.containsKey(a2[i]))
-					m1.put(a2[i], m1.get(a2[i]) + 1);
+				l1.add(one[i]);
+				if( mp.containsKey(two[i]) )
+					mp.put(two[i], mp.get(two[i]) + 1);
 				else
-					m1.put(a2[i], 1);
-			}
-			
+					mp.put(two[i], 1);
+				}
 		}
-		for(int i=0; i < l1.size(); i++){
-			if(m1.containsKey( l1.get(i)) ){
+		for(int i= 0; i < l1.size(); i++){
+			if(mp.containsKey(l1.get(i))){
 				psHit++;
-				if( m1.get(l1.get(i)) > 1 )
-					m1.put(l1.get(i), m1.get(l1.get(i)) - 1);
-				else
-					m1.remove(l1.get(i));
+				int curr = mp.get(l1.get(i));
+				if(curr == 1){
+					mp.put(l1.get(i), 0);
+				}else{
+					mp.put(l1.get(i), curr - 1);
+				}
 			}
+				
 		}
 		
-		System.out.println("Hits "+hit + " Psudeo Hits: "+psHit);
+		System.out.println("PsudeoHit "+ hit);
+		System.out.println("PsudeoHit "+ psHit);
+		
+		
 	}
 }
