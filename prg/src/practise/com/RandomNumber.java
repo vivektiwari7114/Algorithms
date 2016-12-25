88caa919723533e3ac80110d318c96b2792d3236
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-12-26 01:57:46 +0530

    Random Number
    Uniformly generate random number from 1 to 7

--- prg/src/practise/com/RandomNumber.java
+++ prg/src/practise/com/RandomNumber.java
@@ -5,12 +5,15 @@ import java.util.Random;
 public class RandomNumber {
 	public static void main(String args[]){
 		
-			int x = random5()  + (5 * random5() ) - 5;
-			System.out.println(x);
-			while (x > 21);
+		int x =0;
+			do {
+			x = (random5() + 1)  + (5 * random5() );
+			
+			//System.out.println(x);
+			}while (x > 21);
 			
 			
-			System.out.println(x % 7 + 1);
+			System.out.println(x % 7 +1 );
 			
 			
 		
