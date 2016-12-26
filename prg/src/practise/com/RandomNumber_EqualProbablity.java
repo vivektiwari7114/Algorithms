8fb555980ef61d580a7e90967453eb7588c28746
Vivek Tiwari <vivektiwari7114@gmail.com>
2016-12-27 03:19:45 +0530

    Shuffle array or deck of cards with equal probablity

--- prg/src/practise/com/RandomNumber_EqualProbablity.java
+++ prg/src/practise/com/RandomNumber_EqualProbablity.java
@@ -1,5 +1,29 @@
 package practise.com;
 
+import java.util.Random;
+
 public class RandomNumber_EqualProbablity {
+	public static void main(String args[]){
+		
+		int arr[] = {2,-4,5,-7,100};
+		
+		int len = arr.length;
+		Random rm = new Random();
+		
+		for(int i = len; i >0; i--){
+			int index = rm.nextInt(i);
+			System.out.println(index);
+			int temp = arr[index];
+			arr[index] = arr[i-1];
+			arr[i-1] = temp;
+		}
+		
+		for(int k : arr)
+				System.out.print(k + " ");
+		
+		
+	}
+	
+	
 
 }
