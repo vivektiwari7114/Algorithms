602f314517132c12e27924881e32b2e8f37df39a
Vivek Tiwari <vivektiwari7114@gmail.com>
2017-01-18 09:03:39 -0800

    PriorityQueue

--- prg/src/practise/com/checkWorking.java
+++ prg/src/practise/com/checkWorking.java
@@ -5,9 +5,33 @@ import java.util.*;
 public class checkWorking {
 	// Replace Space with %20 
 	public static void main(String args[]){
-		Map<Character, Character> mp = new HashMap<Character, Character>();
-		mp.put('(', ')');
-		mp.put('{', '}');
+		
+		int given[] = {1,2,3,4,5};
+		int k=4;
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(new Comparator<Integer>(){
+			public int compare(Integer a, Integer b){
+				return a-b;
+			}
+			
+		});
+		for(int i=0; i < given.length; i++){
+			pq.offer(given[i]);
+			if(pq.size() > k)
+					pq.poll();
+			
+		}
+		System.out.println("Size "+pq.size());
+		
+		int size = pq.size();
+		
+		for(int i=0; i < size; i++){
+			
+			System.out.print(pq.poll() + " ");
+			
+		}
+		
+		
+		
 		
 		
 		
