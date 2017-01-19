a1d7d1f4fd4800468ae3f115e890bbccb50b491d
Vivek Tiwari <vivektiwari7114@gmail.com>
2017-01-19 09:58:35 -0800

    Random number generator

--- prg/src/practise/com/checkWorking.java
+++ prg/src/practise/com/checkWorking.java
@@ -6,32 +6,12 @@ public class checkWorking {
 	// Replace Space with %20 
 	public static void main(String args[]){
 		
-		int given[] = {1,2,3,4,5};
-		int k=4;
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(new Comparator<Integer>(){
-			public int compare(Integer a, Integer b){
-				return a-b;
-			}
-			
-		});
-		for(int i=0; i < given.length; i++){
-			pq.offer(given[i]);
-			if(pq.size() > k)
-					pq.poll();
-			
+		
+		
+		Random rm = new Random();
+		for(int i=0; i < 10; i++){
+			System.out.println(rm.nextInt(2));
 		}
-		System.out.println("Size "+pq.size());
-		
-		int size = pq.size();
-		
-		for(int i=0; i < size; i++){
-			
-			System.out.print(pq.poll() + " ");
-			
-		}
-		
-		
-		
 		
 		
 		
