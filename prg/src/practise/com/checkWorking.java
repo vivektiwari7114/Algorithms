9c28695c3d1bdc431dabb402234265571a9e2fdc
Vivek Tiwari <vivektiwari7114@gmail.com>
2017-02-09 17:52:27 -0800

    Checking Functionality of Char

--- prg/src/practise/com/checkWorking.java
+++ prg/src/practise/com/checkWorking.java
@@ -6,9 +6,7 @@ public class checkWorking {
 	// Replace Space with %20 
 	public static void main(String args[]){
 		
-		
-		char a = 'a';
-		System.out.println( (char)(a + 25));
+		System.out.println(Math.pow(9, -2));
 		
 		
 	
