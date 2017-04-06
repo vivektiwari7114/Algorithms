c0783b9a50476a525554f75ffcedb73930052bc1
Vivek Tiwari <vivektiwari7114@gmail.com>
2017-04-06 12:45:12 -0700

    Parse Healer Output
    Java Program to parse Healer output

--- prg/src/practise/com/ParseHealerOutput.java
+++ prg/src/practise/com/ParseHealerOutput.java
@@ -6,6 +6,11 @@
 package practise.com;
 import java.io.File;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
 import java.util.*;
 
 
@@ -14,28 +19,52 @@ import java.util.*;
  *
  */
 public class ParseHealerOutput {
+	private static Map<Integer,Integer> store= new HashMap<Integer, Integer>();
 	
 	public static void main(String args[]){
 		
 		String fileName = "data.txt";
 		Scanner inputFile = new Scanner(fileName);
+		readHealerFile(inputFile);
+		
+
+		
+		
+	}
+	public static void readHealerFile ( Scanner inputFile){
+		
+		String pattern = "Action in time step";
+		Pattern pat = Pattern.compile(pattern);
 		try {
-	           
             File file = new File(inputFile.nextLine());
             inputFile = new Scanner(file);
-
-         
             while (inputFile.hasNextLine() ) {
                String line = inputFile.nextLine();
-               
-               System.out.println(line);
-             
+               Matcher match = pat.matcher(line);
+               if(match.find()){
+            	   storeInMap(line);	
+               }
             }
             inputFile.close();
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+	}
+	
+	public static void storeInMap(String line){
+	 String getNumbers[] = line.split(":")[1].split(" ");
+	 for(String num:getNumbers ){
+		 int curr = Integer.parseInt(num);
+		 if(store.containsKey(curr))
+			 	store.put(curr, store.get(curr) + 1 );
+		 else
+			 store.put(curr, 1 );
+			 
+	}
+		 	
+	
+		
 	}
 
 }
