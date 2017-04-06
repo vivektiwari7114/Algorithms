7760f06897d249dc91b39203ae341ad833e672b4
Vivek Tiwari <vivektiwari7114@gmail.com>
2017-04-06 14:35:46 -0700

    ParseHealer output file

--- prg/src/practise/com/ParseHealerOutput.java
+++ prg/src/practise/com/ParseHealerOutput.java
@@ -20,13 +20,19 @@ import java.util.*;
  */
 public class ParseHealerOutput {
 	private static Map<Integer,Integer> store= new HashMap<Integer, Integer>();
+	private static Map<Integer,Integer> mapper= new HashMap<Integer, Integer>();
+	public ParseHealerOutput(){
+		
+		
+	}
 	
 	public static void main(String args[]){
 		
 		String fileName = "data.txt";
 		Scanner inputFile = new Scanner(fileName);
 		readHealerFile(inputFile);
-		
+		store = sortByValue(store);
+		System.out.println(store);
 
 		
 		
@@ -53,18 +59,34 @@ public class ParseHealerOutput {
 	}
 	
 	public static void storeInMap(String line){
-	 String getNumbers[] = line.split(":")[1].split(" ");
+	 String getNumbers[] = line.split(":")[1].trim().split(" ");
 	 for(String num:getNumbers ){
+		if( !num.equals(" ")){
+			//System.out.println("Get: "+num);
 		 int curr = Integer.parseInt(num);
 		 if(store.containsKey(curr))
 			 	store.put(curr, store.get(curr) + 1 );
 		 else
 			 store.put(curr, 1 );
-			 
+		}
+	 }	
 	}
-		 	
+	public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+		List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
+		Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+			@Override
+			public int compare(Map.Entry<K, V> e1, Map.Entry<K, V> e2) {
+				return (e1.getValue()).compareTo(e2.getValue());
+			}
+		});
+	 
+		Map<K, V> result = new LinkedHashMap<>();
+		for (Map.Entry<K, V> entry : list) {
+			result.put(entry.getKey(), entry.getValue());
+		}
+	 
+		return result;
+	}
 	
-		
-	}
 
 }
