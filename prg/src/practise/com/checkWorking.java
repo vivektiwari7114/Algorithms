806ff05b0d721efa9e82d9cec162d07a7c02d671
Vivek Tiwari <vivek.tiwari@Vivek-Tiwaris-MacBook-Pro.local>
2017-05-23 13:50:06 -0700

    Update the comments section

--- prg/src/practise/com/checkWorking.java
+++ prg/src/practise/com/checkWorking.java
@@ -2,6 +2,7 @@ package practise.com;
 
 	public class checkWorking{
 		public static void main(String args[]){
+			//Initiliazing the nums array
 			int nums[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 			System.out.println( PredictTheWinner(nums) );
 			
